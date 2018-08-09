6aa4ba147a526e14017d3019032ee12ff4bb7be1
LXW <704474965@qq.com>
2018-08-09 09:48:24 +0800

    Initial commit

---
+++ MyFirstDemo/src/com/lxw/test/seconDemo.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (C), 2018-2018, 微应科技有限公司
+ * FileName: seconDemo
+ * Author:   Jerry
+ * Date:     2018/7/30 17:17
+ * Description: idea新建的第二个demo类
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.lxw.test;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈idea新建的第二个demo类〉
+ *
+ * @author Jerry
+ * @create 2018/7/30
+ * @since 1.0.0
+ */
+public class seconDemo {
+
+    public static final int NUM = 1;
+
+    public static void main(String args[]){
+
+        System.out.println("第二个Demo类");
+    }
+
+
+    public void main(){
+
+        main();
+    }
+
+}
